00b6ff2f1491915d7e6632104b56e2ddce65c6e8
ChengX‘ <1056421380@qq.com>
2018-10-09 09:44:25 +0800

    commit all file

---
+++ src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p class="card">
+      {{text}}
+    </p>
+    <i-button type="primary" bind:click="handleClick">这是一个按钮</i-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+}
+</script>
+
+<style>
+.card {
+  padding: 10px;
+}
+</style>
